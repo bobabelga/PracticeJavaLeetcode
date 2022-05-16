c23bbf4c05c4d27451d39d146eddbf2d5ef20012
Rahimo Kasimo <rahimokasimo@gmail.com>
2022-05-16 11:04:41 +0100

    Practice

---
+++ src/com/bobabelga/mergeTwoSortedLists/MergeNames.java
@@ -0,0 +1,25 @@
+package com.bobabelga.mergeTwoSortedLists;
+
+
+import java.util.*;
+
+public class MergeNames {
+
+    public static String[] uniqueNames(String[] names1, String[] names2) {
+        List<String> uniqueNamesList = Arrays.asList(names1);
+        ArrayList<String> uniqueNamesArrayList = new ArrayList<>(uniqueNamesList);
+        for (String name : names2)
+            if (!uniqueNamesList.contains(name))
+                uniqueNamesArrayList.add(name);
+        Set<String> hSet = new HashSet<>(uniqueNamesArrayList);
+
+
+        return hSet.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        String[] names1 = new String[]{"Ava", "Emma", "Olivia","Ava"};
+        String[] names2 = new String[]{"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
---
+++ src/com/bobabelga/mergeTwoSortedLists/UserInput.java
@@ -0,0 +1,36 @@
+package com.bobabelga.mergeTwoSortedLists;
+
+public class UserInput {
+
+    public static class TextInput {
+        String input = "";
+
+        public void add(char c) {
+            input = input + c;
+        }
+
+        public String getValue() {
+            return input;
+        }
+    }
+
+    public static class NumericInput extends TextInput {
+        @Override
+        public void add(char c) {
+            try {
+                int number = Integer.parseInt(String.valueOf(c));
+                super.add(c);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TextInput input = new NumericInput();
+        input.add('1');
+        input.add('a');
+        input.add('0');
+        System.out.println(input.getValue());
+    }
+}
